340157d49392ea182f983576183d6bbc31d51eee
Bayu Saputra <babahayoo@gmail.com>
2019-12-01 23:32:21 -0800

    adding searchview on code but not works yet

--- app/src/main/java/com/example/kadesubmisidua/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/kadesubmisidua/view/activity/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.kadesubmisidua.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import com.example.kadesubmisidua.R
 import com.example.kadesubmisidua.view.fragment.NextFragment
@@ -55,4 +58,31 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         return loadFragment(selectedFragment)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_option_menu,menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu?.findItem(R.id.main_option_search)?.actionView as androidx.appcompat.widget.SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        searchView.queryHint = "Cari Pertandingan"
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.d("MainActivity", "onQuerySubmit val : " + query);
+
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                Log.d("MainActivity", "onQueryChange val : " + newText);
+
+                return true
+            }
+
+        })
+
+        return true
+    }
 }
